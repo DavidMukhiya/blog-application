d9775af882ba82a4530bfcc1533e0503d61d860e
David Prakash Mukhiya <sherlock.41v94@gmail.com>
2023-02-27 01:07:39 -0800

    implement Jwt Token

--- blog-application-backend/src/main/java/com/david/blogapplication/config/SecurityConfig.java
+++ blog-application-backend/src/main/java/com/david/blogapplication/config/SecurityConfig.java
@@ -1,21 +1,32 @@
 package com.david.blogapplication.config;
 
 import com.david.blogapplication.security.CustomUserDetailService;
+import com.david.blogapplication.security.JwtAuthenticationEntryPoint;
+import com.david.blogapplication.security.JwtAuthenticationFilter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig  extends WebSecurityConfigurerAdapter {
     @Autowired
     private CustomUserDetailService customUserDetailService;
+
+    @Autowired
+    private JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
+
+    @Autowired
+    private JwtAuthenticationFilter jwtAuthenticationFilter;
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
@@ -25,12 +36,18 @@ public class SecurityConfig  extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.
-                csrf().disable().
-                authorizeHttpRequests()
+                csrf().disable()
+                .authorizeHttpRequests()
+                .antMatchers("/api/auth/login").permitAll()
                 .anyRequest()
                 .authenticated()
                 .and()
-                .httpBasic();
+                .exceptionHandling().authenticationEntryPoint(this.jwtAuthenticationEntryPoint)
+                .and()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+
+        http.addFilterBefore(this.jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
     }
 
     @Bean
@@ -38,5 +55,9 @@ public class SecurityConfig  extends WebSecurityConfigurerAdapter {
         return new BCryptPasswordEncoder();
     }
 
-
+    @Bean
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
 }
---
+++ blog-application-backend/src/main/java/com/david/blogapplication/payloads/JwtAuthResponse.java
@@ -0,0 +1,8 @@
+package com.david.blogapplication.payloads;
+
+import lombok.Data;
+
+@Data
+public class JwtAuthResponse {
+    private String token;
+}
