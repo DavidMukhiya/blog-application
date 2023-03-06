b6c198061038b10ede254447b5b2626f5205ae66
David Prakash Mukhiya <sherlock.41v94@gmail.com>
2023-03-05 20:41:48 -0800

    added login form

--- blog-application-frontend/src/pages/Login.js
+++ blog-application-frontend/src/pages/Login.js
@@ -1,8 +1,43 @@
 import React from 'react'
+import { Container, Row, Col, Card, CardHeader, CardBody, Form, FormGroup, Label, Input, Button } from 'reactstrap'
 
 const Login = () => {
   return (
-    <div>Login</div>
+    <Container>
+      <Row className='mt-4' >
+        <Col lg={{size:6, offset:3}} >
+          <Card color='dark' inverse>
+            <CardHeader>
+             <h3>Login Here !!</h3>
+            </CardHeader>
+            <CardBody>
+              <Form>
+                {/* Email Field */}
+                <FormGroup>
+                  <Label for='email' >
+                    Enter Email
+                  </Label>
+                  <Input type='text' id='email'/>
+                </FormGroup>
+
+                {/* Password Field */}
+                <FormGroup>
+                  <Label for='password'>
+                    Enter Password
+                  </Label>
+                  <Input type='password' id='password' />
+                </FormGroup>
+
+                <Container className='text-center'>
+                  <Button outline color='light' >Login</Button>
+                  <Button className='ms-2' color='secondary' type='reset' outline>Reset</Button>
+                </Container>
+              </Form>
+            </CardBody>
+          </Card>
+        </Col>
+      </Row>
+    </Container>
   )
 }
 
