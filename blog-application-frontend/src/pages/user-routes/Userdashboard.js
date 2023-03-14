06829a1360316c3f2de45b0883f92a96bb8b041c
David Prakash Mukhiya <sherlock.41v94@gmail.com>
2023-03-13 23:07:20 -0700

    added post form, category is not rendering properly

--- blog-application-frontend/src/pages/user-routes/Userdashboard.js
+++ blog-application-frontend/src/pages/user-routes/Userdashboard.js
@@ -1,18 +1,12 @@
 import React from "react";
+import { Container } from "reactstrap";
+import AddPost from "../../components/AddPost";
 
 const Userdashboard = () => {
   return (
-    <div>
-      <h1>Welcome to user dashboard</h1>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab aliquid
-        repellat veritatis in provident explicabo itaque enim numquam unde
-        placeat. Voluptate velit perspiciatis consequuntur commodi odit
-        excepturi facere molestiae, nam quam odio illo eaque aut fugiat non
-        nulla dolores eos necessitatibus cumque consectetur illum dolorem, quasi
-        unde? Rem, deserunt quibusdam.
-      </p>
-    </div>
+    <Container>
+      <AddPost />
+    </Container>
   );
 };
 
